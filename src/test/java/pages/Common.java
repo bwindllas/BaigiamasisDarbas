5807b1b74fd5ebb1a6992563a38fcd475f7c8318
bwindllas <arlauskas.edgaras@yahoo.com>
2022-05-26 13:36:57 +0100

    Update Common class

--- src/test/java/pages/Common.java
+++ src/test/java/pages/Common.java
@@ -1,10 +1,27 @@
 package pages;
 
-import utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 
 public class Common {
 
     public static void openUrl(String url) {
-        Driver.getDriver().get(url);
+        utils.Driver.getDriver().get(url);
+    }
+
+    public static WebElement getElement(By locator) {
+        return utils.Driver.getDriver().findElement(locator);
+    }
+
+    public static void sendKeysToElement(By locator, String keys) {
+        getElement(locator).sendKeys(keys);
+    }
+
+    public static void clickElement(By locator) {
+        getElement(locator).click();
+    }
+    public static String getElementText(By locator) {
+        return getElement(locator).getText();
     }
 }
