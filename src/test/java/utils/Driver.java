4c79a8847dd2b2411578cdfda569c78ffbc27d68
bwindllas <arlauskas.edgaras@yahoo.com>
2022-05-26 13:35:44 +0100

    Update driver class

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -3,20 +3,27 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import pages.Constants;
 
 public class Driver {
-    private static WebDriver driver;
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static void setDriver() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver.set(new ChromeDriver(options));
+        driver.get().manage().timeouts().implicitlyWait(Constants.TIMEOUT);
     }
 
     public static WebDriver getDriver() {
-        return driver;
+        return driver.get();
     }
 
     public static void closeDriver() {
-        driver.quit();
+        driver.get().quit();
+        driver.remove();
     }
 }
